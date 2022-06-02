54f2bd38d9629bbafe0535f2f1efb31d8bd1b3af
Ynara lira ventura <79769248+ynaralira@users.noreply.github.com>
2022-06-02 10:27:39 -0300

    Create MultiploDeDez.java

---
+++ MultiploDeDez.java
@@ -0,0 +1,12 @@
+public class MultiploDeDez {
+	  public static void main(String[] args){       
+		int n ;
+		n= 0;
+
+		for (int i = 0; i < 100; i++){
+		if ((i % 10) == 0){
+		System.out.println("Multiplo de 10");
+		}
+		}
+		}
+		}
