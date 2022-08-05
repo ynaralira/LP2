df9f5c727bb9092e205e3d272f35ab32aa9b13c4
Ynara lira ventura <79769248+ynaralira@users.noreply.github.com>
2022-08-05 08:29:43 -0300

    Create Bicicleta.java

---
+++ Bicicleta.java
@@ -0,0 +1,14 @@
+public class Bicicleta extends Veiculo{
+
+
+    public Bicicleta() {
+        this.setQtdePneus(2);
+    }
+
+    public Bicicleta bicicleta(){
+        Bicicleta bicicleta = new Bicicleta();
+        return bicicleta;
+    }
+
+}
+
